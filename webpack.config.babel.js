fbfbf9d26fcb80290f59970e3900ed0ac0c1f5a9
Carlos Kazuo Mochizuki Ishizaka <carlos.kazuo@grupofolha.com.br>
2016-02-19 01:10:41 -0200

    Refactored webpack.config.babel.js

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -5,89 +5,107 @@ import autoprefixer from 'autoprefixer';
 import { config as pkgConfig } from './package.json';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 
-const config = (env, common) => {
-  return Object.assign(common, env === 'production' ? {
-    plugins: [
-      ...common.plugins
-      new webpack.optimize.DedupePlugin(),
-      new webpack.optimize.OccurrenceOrderPlugin(),
-      new webpack.optimize.UglifyJsPlugin({
-        mangle: true,
-        output: {
-          comments: false
-        },
-        compress: {
-          warnings: false
-        }
-      })
-    ]
-  } : {
-    entry: [
+export default (
+  (env, common) =>
+    env === 'production' ?
+
+    // production only
+    {
+      ...common,
+
+      devtool: 'source-map',
+
+      plugins: [
+        ...common.plugins,
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+          mangle: true,
+          output: {
+            comments: false
+          },
+          compress: {
+            warnings: false
+          }
+        })
+      ]
+    }
+
+    :
+
+    // development only
+    {
+      ...common,
+
       debug: true,
       cache: true,
+      devtool: 'inline-source-map',
 
-      common.entry,
-      util.format('webpack-dev-server/client?http://%s:%d', pkgConfig.devServerHost, pkgConfig.devServerPort),
-      'webpack/hot/dev-server'
-    ],
+      entry: [
+        common.entry,
+        util.format('webpack-dev-server/client?http://%s:%d', pkgConfig.devServerHost, pkgConfig.devServerPort),
+        'webpack/hot/dev-server'
+      ],
+
+      plugins: [
+        ...common.plugins,
+        new HtmlWebpackPlugin()
+      ],
+
+      devServer: {
+        contentBase: path.resolve(__dirname, pkgConfig.buildDir),
+        hot: true,
+        noInfo: false,
+        inline: true,
+        stats: { colors: true }
+      }
+    }
+)(
+  process.env.NODE_ENV || 'development',
+
+  // config for both development and production
+  {
+    entry: './index.js',
+
+    output: {
+      path: path.resolve(__dirname, pkgConfig.buildDir),
+      filename: 'bundle.js',
+      publicPath: '/'
+    },
+
+    target: 'web',
+
+    resolve: {
+      extensions: ['', '.js', '.jsx', 'styl'],
+
+      alias: {
+        app: path.resolve(__dirname, 'app'),
+        components: 'app/components',
+        libs: 'app/libs'
+      }
+    },
+
+    module: {
+      loaders: [
+        { test: /\.jsx?/, loader: 'babel', query: { presets: [ 'es2015', 'react', 'stage-0' ] }, exclude: /node_modules/, include: /app/ },
+        { test: /\.styl/, loader: 'style!css?sourceMap&minimize!postcss!stylus' },
+        { test: /\.jpe?g$|\.gif$|\.png$|\.ico|\.svg$|\.woff$|\.ttf$/, loader: 'file?name=[path][name].[ext]' }
+      ]
+    },
 
     plugins: [
-      ...common.plugins,
-      new HtmlWebpackPlugin()
+      new webpack.DefinePlugin({
+        'process.env': { 'NODE_ENV': JSON.stringify(process.env.NODE_ENV) },
+        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      })
     ],
 
-    devServer: {
-      contentBase: path.resolve(__dirname, pkgConfig.buildDir),
-      hot: true,
-      noInfo: false,
-      inline: true,
-      stats: { colors: true }
-    }
-  });
-};
+    babel: {
+      presets: [ 'es2015', 'react', 'stage-0' ]
+    },
 
-export default config(process.env.NODE_ENV || 'development', {
-  entry: './index.js',
-
-  output: {
-    path: path.resolve(__dirname, pkgConfig.buildDir),
-    filename: 'bundle.js',
-    publicPath: '/'
-  },
-
-  target: 'web',
-  devtool: process.env.NODE_ENV === 'production' ? 'source-map' : 'inline-source-map',
-
-  resolve: {
-    extensions: ['', '.js', '.jsx', 'styl'],
-
-    alias: {
-      app: path.resolve(__dirname, 'app'),
-      components: 'app/components',
-      libs: 'app/libs'
-    }
-  },
-
-  module: {
-    loaders: [
-      { test: /\.jsx?/, loader: 'babel', query: { presets: [ 'es2015', 'react', 'stage-0' ] }, exclude: /node_modules/, include: /app/ },
-      { test: /\.styl/, loader: 'style!css?sourceMap&minimize!postcss!stylus' },
-      { test: /\.jpe?g$|\.gif$|\.png$|\.ico|\.svg$|\.woff$|\.ttf$/, loader: 'file?name=[path][name].[ext]' }
+    postcss: [
+      autoprefixer
     ]
-  },
-
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': { 'NODE_ENV': JSON.stringify(process.env.NODE_ENV) },
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-    })
-  ],
-
-  babel: {
-    presets: [ 'es2015', 'react', 'stage-0' ]
-  },
-
-  postcss: [
-    autoprefixer
-  ]
-});
+  }
+);
